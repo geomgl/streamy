43c96eada29e08a1104c80df94d8216d0c4e2b13
George Magloire <gmagloire22@gmail.com>
2020-03-20 17:22:49 -0400

    added api and rtmp directories

---
+++ rtmpserver/index.js
@@ -0,0 +1,21 @@
+// this server makes video available to the browser on port 8000.  It also listens for incoming
+// streams on port 1935
+
+const NodeMediaServer = require('node-media-server');
+
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+  http: {
+    port: 8000,
+    allow_origin: '*'
+  }
+};
+
+var nms = new NodeMediaServer(config)
+nms.run()
